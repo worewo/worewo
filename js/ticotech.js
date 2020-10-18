2fa175ce90d8f540e93d8c9244365370e2c8c6a0
worewo <72999886+worewo@users.noreply.github.com>
2020-10-18 12:29:21 +0530

    Add files via upload

--- js/ticotech.js
+++ js/ticotech.js
@@ -15,36 +15,3 @@ typewriter.typeString('Information')
     .typeString('Technology')
     .pauseFor(2500)
     .start();
-
-let userlist=[];
-const adduser = (data)=>{
-    data.preventDefault();
-    let user = {
-        name: document.querySelector("#name").value,
-        email: document.querySelector("#email").value
-    }
-    userlist.push(user);
-    document.forms[0].reset();
-
-    localStorage.setItem('WoreWouserlist', JSON.stringify(userlist));
-}
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    document.querySelector('#submit').addEventListener('click', adduser);
-});
-
-
-//--------------------form submit alert popup design-----------------------------
-
-  var formsubmit = document.getElementById("form_submit");
-
- formsubmit.onclick = function () {
-   alert("Thank You for Your Submission :) You'll Receive a Confirmation Message Shortly   ");
-    }
-    
-//--------------------form submit alert popup design-----------------------------
-
-
-
-
-
