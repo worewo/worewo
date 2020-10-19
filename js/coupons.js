c727243b2cc4123c9b826c3c00b44b14074a925c
worewo <72999886+worewo@users.noreply.github.com>
2020-10-19 21:00:54 +0530

    Add files via upload

---
+++ js/coupons.js
@@ -0,0 +1,7 @@
+AOS.init({duration:1200,})
+
+let play = document.querySelector("#demo");
+play.addEventListener('click', ()=>{
+    let video = document.querySelector("#video")
+    video.style.display = "flex";
+});
